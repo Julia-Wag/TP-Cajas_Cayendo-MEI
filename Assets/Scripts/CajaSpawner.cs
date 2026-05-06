2531886beafe924f59b30c59deeeb32495db9f9d
Julia-Wag <juli.w.081009@gmail.com>
2026-05-06 14:24:54 -0300

    Initial commit

---
+++ Assets/Scripts/CajaSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaSpawner : MonoBehaviour
+{
+    int randomNumber;
+    public float startYValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LocateBoxAtRandomPosition();
+    }
+
+    public void LocateBoxAtRandomPosition()
+    {
+        Vector3 currentPosition = transform.position;
+        float newX = 0;
+        randomNumber = Random.Range(0,3);
+        if (randomNumber == 0)
+        {
+            newX = -2f;
+        }
+        else if(randomNumber == 1)
+        {
+            newX = 0f;
+        }
+        else if(randomNumber == 2)
+        {
+            newX = 2f;
+        }
+        currentPosition.x = newX;
+        currentPosition.y = startYValue;
+        transform.position = currentPosition;
+        //Tarea 2: cuando la caja toque el piso no se destruya, se repocisiona, usando esta funcion
+    }
+}
