5e1a2d3fdefbabf754de9f8d8e7deb27dc39ac65
Julia-Wag <juli.w.081009@gmail.com>
2026-05-08 08:59:28 -0300

    MESALIOSINCHAT

---
+++ Assets/Scripts/ScriptHelices/HeliceCollition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliceCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Helice"))
+        {
+            col.gameObject.GetComponent<CajaSpawner>().LocateBoxAtRandomPosition();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
